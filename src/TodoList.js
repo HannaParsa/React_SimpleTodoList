39ee346f210fa810aa8e61014c2972957cf012ed
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-03-31 09:51:56 +0330

    a simple todoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,36 +1,43 @@
-import React, {Component} from 'react';
-import TodoItems from './TodoItems';
- class TodoList extends Component{
-     constructor(props){
-         super(props);
-         this.state = {items: []};
-         this.addItem = this.addItem.bind(this);
-         this.deleteItem = this.deleteItem.bind(this);
-     }
-     addItem(e){
-            if(this._inputElement.value !== ""){
-               var newItem = {
-                   text: this._inputElement.value,
-                   key: Date.now()
-               }
-               this.setState((prevstate)=> {
-                  return{
-                      items: prevstate.items.concat(newItem)
-                  }
-               });
-            }
-            e.preventDefault();
-            //will not add to list
-     }
-     deleteItem(key){
-        var filteredItems = this.state.items.filter(function(item) {
+import React, {Component} from "react";
+import TodoItems from "./TodoItems";
+
+class TodoList extends Component {
+	constructor(props){
+		super(props);
+		this.state = {items: []};
+		this.addItem = this.addItem.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+	addItem(e){
+		if(this._inputElement.value !== ""){
+			var newItem = {
+				text: this._inputElement.value, 
+				key: Date.now()
+			};
+	
+			this.setState((prevState) => {
+				return {
+					items: prevState.items.concat(newItem)
+				};
+			});
+		
+			this._inputElement.value = "";
+		}
+		console.log(this.state.items);
+		e.preventDefault();
+	}
+	deleteItem(key){
+		var filteredItems = this.state.items.filter(function(item) {
 			return (item.key !== key);
 		});
-         this.setState({items: filteredItems});
-     }
-     render(){
-         return(
-            <div className="todoListMain">
+		
+		this.setState({
+			items: filteredItems
+		});
+	}
+	render(){
+		return(
+			<div className="todoListMain">
 			  <div className="header">
 			    <form onSubmit={this.addItem}>
 			      <input ref={(a) => this._inputElement = a} placeholder="enter task">
@@ -39,7 +46,9 @@ import TodoItems from './TodoItems';
 			    </form>
 			    <TodoItems entries={this.state.items} delete={this.deleteItem} />
 			  </div>
-		    </div>
-         )
-     }
- }
+		        </div>
+		);
+	}
+}
+
+export default TodoList; 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import TodoList from "./TodoList";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <div>
+	<TodoList/>
+  </div>, 
   </React.StrictMode>
 );
 
@@ -15,3 +18,30 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+var destination = document.querySelector('#container');
+
+
+ReactDOM.render(
+  <div>
+	<TodoList/>
+  </div>, 
+  destination
+);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoList from "./TodoList";
+
+
+var destination = document.querySelector('#container');
+
+
+ReactDOM.render(
+  <div>
+	<TodoList/>
+  </div>, 
+  destination
+);
