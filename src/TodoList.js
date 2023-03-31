1c2c178e330baa04365d9f54fcace728de322434
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-03-31 09:33:07 +0330

    in progress

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,9 +8,29 @@ import TodoItems from './TodoItems';
          this.deleteItem = this.deleteItem.bind(this);
      }
      addItem(e){
-
+            if(this._inputElement.value !== ""){
+               var newItem = {
+                   text: this._inputElement.value,
+                   key: Date.now()
+               }
+               this.setState((prevstate)=> {
+                  return{
+                      items: prevstate.items.concat(newItem)
+                  }
+               });
+            }
+            e.preventDefault();
+            //will not add to list
      }
      deleteItem(key){
+        var filteredItems = this.state.items.filter(function(item) {
+			return (item.key !== key);
+		});
+         this.setState({items: filteredItems});
+     }
+     render(){
+         return(
 
+         )
      }
  }
